d70059f6df250db5b645634d14fefc16ff2f8040
qichunren <whyruby@gmail.com>
2024-10-21 20:16:09 +0800

    npx shadcn@latest add switch

--- app/frontend/components/app.jsx
+++ app/frontend/components/app.jsx
@@ -1,13 +1,14 @@
 import React, { createContext } from 'react';
-
+import { Switch } from './ui/switch';
 const App = () => {
   return (
     <div className="container mx-auto p-4">
       <div className='text-4xl font-bold'>Rails React Starter</div>
       <header>
-        <ul className='flex gap-4 bg-gray-500 p-4'>
+        <ul className='flex gap-4 p-4 items-center'>
           <li><a href='/'>Home</a></li>
           <li><a href='/posts'>Posts</a></li>
+          <Switch />
         </ul>
       </header>
     </div>
