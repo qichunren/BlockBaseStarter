4a8a11cc404b3a43d255390127e1e36240783ce1
Qichunren <39784+qichunren@users.noreply.github.com>
2024-11-13 17:43:58 +0000

    Seprate common rails layout  from react app layout

--- app/frontend/entrypoints/application_react.tsx
+++ app/frontend/entrypoints/application_react.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client';
-import '../css/index.css';
 import App from '../components/app'
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('root');
-  const root = createRoot(container);
-  root.render(<App />);
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  }
 })
