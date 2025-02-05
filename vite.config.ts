4b1285af7b0fce9e6d8a986348a54ef7c0749c28
qichunren <whyruby@gmail.com>
2025-02-05 20:37:54 +0800

    Upgrade tailwindCSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "app/frontend"),
